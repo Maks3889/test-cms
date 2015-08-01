b8f4a0bb19dc34524461fa0f5e2b0a1fe8bcf910
Maks3889 <Maks_3889@mail.ru>
2015-08-01 17:45:33 +0300

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,3 +1,17 @@
 <?php
 
-phpinfo();
+class Article {
+    public $title;
+    public $text;
+    public function view() {
+        echo $this->title;
+    }
+}
+
+class News extends Article {
+
+}
+
+$a = new News();
+$a->title = 'Header';
+$a->view();
